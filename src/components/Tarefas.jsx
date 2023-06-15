3c49dbea10a9371f381ba02d0c2f5e1ac3c2ee6c
Paulo de Tassio Damasceno <damascenopaulo450@gmail.com>
2023-06-15 15:51:39 -0300

    card das tarefas

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Navbar from './NavBar'
 import Card from './Card'
-
+import '../styles/main.scss'
 const Tarefas = () => {
   return (
     <div>
       <Navbar/>
-      <main>
+      <main >
         <h1>Suas Tarefas</h1>
         <section>
-          <ul>
-            <li><Card/></li>
-            <li><Card/></li>
+          <ul className='card-list'>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
+            <li className='item'><Card/></li>
           </ul>
         </section>
       </main>
